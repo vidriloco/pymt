2147881dfc5c674e3a8e629b539a94d264ed1893
Alejandro E. Cruz Paz <vidriloco@gmail.com>
2010-02-11 01:11:13 -0600

    Previous commit suggestions are now part of this commit with the exception of tito's suggestion on the MT3DWorld number method

--- examples/3Dselection/mt3DWorld.py
+++ examples/3Dselection/mt3DWorld.py
@@ -1,7 +1,7 @@
 from pymt import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from OpenGL.GLUT import *
+import random
 # Change the following import to your own mt3DObject implementation
 # IF you are declaring the __main__ here
 from reactive3DObject import *
@@ -9,9 +9,14 @@ from reactive3DObject import *
 class MT3DWorld(MTWidget):
     def __init__(self, **kwargs):
     	super(MT3DWorld, self).__init__(**kwargs)
+
+        kwargs.setdefault('buffer_size', 64)
+        kwargs.setdefault('pick_size', 2)
+
+
     	self.on_selection   = False
-    	self._pick_size     = 2
-    	self._pick_buffer   = glSelectBuffer(64)
+    	self._pick_size     = kwargs.get('pick_size')
+    	self._pick_buffer   = glSelectBuffer(kwargs.get('buffer_size'))
         
         self.touch_position = {}
         self.rotation_matrix = None
@@ -24,22 +29,16 @@ class MT3DWorld(MTWidget):
     	self.count = 0
     
     def reset_rotation(self):
-        glMatrixMode(GL_MODELVIEW)
-        glPushMatrix()
-        glLoadIdentity()
-        self.rotation_matrix = glGetFloatv(GL_MODELVIEW_MATRIX)
-        glPopMatrix()
+        with gx_matrix:
+            self.rotation_matrix = glGetFloatv(GL_MODELVIEW_MATRIX)
     
     def rotate_scene(self, x,y,z):
-        glMatrixMode(GL_MODELVIEW)
-        glPushMatrix()
-        glLoadIdentity()
-        glRotatef(z, 0,0,1)
-        glRotatef(x, 0,1,0)
-        glRotatef(y, 1,0,0)
-        glMultMatrixf(self.rotation_matrix)
-        self.rotation_matrix = glGetFloatv(GL_MODELVIEW_MATRIX)
-        glPopMatrix()
+        with gx_matrix_identity:
+            glRotatef(z, 0,0,1)
+            glRotatef(x, 0,1,0)
+            glRotatef(y, 1,0,0)
+            glMultMatrixf(self.rotation_matrix)
+            self.rotation_matrix = glGetFloatv(GL_MODELVIEW_MATRIX)
     
     def on_touch_down(self, touch):
     	self.do_pick(touch.x, touch.y)
@@ -58,7 +57,7 @@ class MT3DWorld(MTWidget):
         
         dx, dy, angle = 0,0,0
         #two touches:  scale and rotate around Z
-        if  self.touch_position.has_key(self.touch1) and self.touch_position.has_key(self.touch2):
+        if  self.touch1 in self.touch_position and self.touch2 in self.touch_position:
             v1 = Vector(*self.touch_position[self.touch1])
             v2 = Vector(*self.touch_position[self.touch2])
             
@@ -87,24 +86,36 @@ class MT3DWorld(MTWidget):
     
     def do_pick(self, x, y):
     	self.on_selection = True
+        # selection initialization
     	self.pick_start(x, y)
+        # fake draw because we are draing to the picking area
     	self.draw()
+        # restore matrix mode and recover buffer selection results
     	self.pick_stop()
     	self.on_selection = False
     
     def pick_start(self, x, y):
+        
         viewport = glGetIntegerv(GL_VIEWPORT)
         width, height = getWindow().size
-        
+        # change to selection mode, so we can get the objects hits in the select buffer 
         glRenderMode(GL_SELECT)
+        # initialize the names stack (a stack where the objects names will be
+        # pushed everytime we draw one on selection mode)
         glInitNames()
+        # Put something on the stack, so we can replace it with another object
+        # name. On the draw method we use glLoadName(name) which makes the
+        # replacement of the very initial object. We could have used
+        # glPushName(name) and then glPopName() also.
         glPushName(0)
         
+        # we now prepare for 'rendering' to the picking area of the size of the
+        # pick_size from the saved viewport on position x,y
         glMatrixMode(GL_PROJECTION)
         glPushMatrix()
         glLoadIdentity()
+        # restricts drawing to a region of the saved viewport
         gluPickMatrix(x, y, self._pick_size, self._pick_size, viewport)
-        # left, right, bottom, top, NEAR, FAR
         glFrustum(-width / 2, width / 2, -height / 2, height / 2, .1, 1000)
         glTranslatef(-x, -y, 0.)
         glScalef(5000, 5000, 1)
@@ -112,6 +123,7 @@ class MT3DWorld(MTWidget):
         glMatrixMode(GL_MODELVIEW)
     
     def pick_stop(self):
+        # returns the hits recorded in the select buffer
     	hits = glRenderMode(GL_RENDER)
     	self.process_hits(hits, self._pick_buffer)
     	
@@ -120,27 +132,30 @@ class MT3DWorld(MTWidget):
     	glMatrixMode(GL_MODELVIEW)
 	
     def draw(self):
-      
-        glPushMatrix()
-    	glMultMatrixf(self.rotation_matrix)
-        # Centering and rotating the world
-        glTranslate(self.width/2, self.height/2, 10)
-        glScalef(15, 15, 15)
-        glRotate(90.0, self.width/2, self.height/2, 0)
+        with gx_matrix: 
+    	    glMultMatrixf(self.rotation_matrix)
+            # Centering and rotating the world
+            glTranslate(self.width/2, self.height/2, 10)
+            glScalef(15, 15, 15)
+            glRotate(90.0, self.width/2, self.height/2, 0)
     	
-        glScalef(self.zoom, self.zoom, self.zoom)
-        # then we draw every object in all the registered scenes
-    	for model_key in self.models.keys():
-            for obj in self.models[model_key]:
-                if self.on_selection:
-                    glLoadName(obj.get_number())
-                obj.draw()
-        glPopMatrix()
+            glScalef(self.zoom, self.zoom, self.zoom)
+            # then we draw every object in all the registered scenes
+    	    for model_key in self.models.keys():
+                for obj in self.models[model_key]:
+                    # This is true whenever a on_touch_down event is fired
+                    if self.on_selection:
+                        # Before drawing an object we put it's ID so we can
+                        # recognize it once we touch it
+                        glLoadName(obj.get_number())
+                    obj.draw()
 
     def register_objects(self, group, object_list):
+        # register a group of objects by name
     	self.models[group] = object_list
     
     def number(self):
+        # global object counter (for each registered object ID assignment)
     	current_count = self.count
     	self.count+=1
     	return current_count
@@ -153,6 +168,7 @@ class MT3DWorld(MTWidget):
     	latest = None
     	last_near = -1
     	
+        # analize hits. We will keep the nearest hit towards the user.
     	for hit in hits:
             if last_near == -1 or (hit.near < last_near):
                 last_near = hit.near
@@ -182,13 +198,13 @@ if __name__ == '__main__':
     objects = OBJ("test_scene.obj").get_objects()
     list = []
     
-    for index, obj in enumerate(objects):
+    for obj in objects:
         # Subclassing instance of an mt3DOBject.
         # Always remember to pass the world object this particular
         # scene object will be registered in as it'll be crucial for object identification by openGL
         
         # example: generating a color for each object
-        rgb_color = (index*.3, index*.5, index*.1)
+        rgb_color = (random.random(), random.random(), random.random())
 
         threeDobj = Reactive3DObject(obj, world.number(), rgb=rgb_color)
         list.append(threeDobj)
--- examples/3Dselection/reactive3DObject.py
+++ examples/3Dselection/reactive3DObject.py
@@ -1,22 +1,24 @@
 from mt3DObject import *
+from pymt import *
 
 class Reactive3DObject(MT3DObject):
     
     def __init__(self, mesh, number, **kwargs):
         MT3DObject.__init__(self,mesh, number, **kwargs)
-        self.active = kwargs.get('active')
         kwargs.setdefault('active', False)
         kwargs.setdefault('rgb', (1.0, 1.0, 1.0))
+
+        self.active = kwargs.get('active')
         # Can set a color for this object 
         self.rgb = kwargs.get('rgb')
 
     def draw(self):
         if self.active:
-            prev_color = glGetFloatv(GL_CURRENT_COLOR)
-            glColor(self.rgb[0], self.rgb[1], self.rgb[2])
-        MT3DObject.draw(self)
-        if self.active:
-            glColor(prev_color[0], prev_color[1], prev_color[2], prev_color[3])
+            with gx_color(*self.rgb):
+                MT3DObject.draw(self)
+        else:
+            with gx_color(1,0,0,0.5):
+                MT3DObject.draw(self)
     # Define here what to do when your object is touched        
     def touched(self):
         self.active = not self.active
